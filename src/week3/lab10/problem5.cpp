ef3e4efabeff07c77d4a02e8d0f06a5c26aa8ee0
rustamtursunkulov <rustamtursunkulov02@gmail.com>
2025-06-20 17:11:12 +0500

    finished lab10

--- src/week3/lab10/problem5.cpp
+++ src/week3/lab10/problem5.cpp
@@ -1,18 +1,24 @@
 #include <iostream>
 using namespace std;
 
-int indexOfLargestElement(double arr[], int size) {
-    int index = 0;
-    for(int i = 1; i < size; i++)
-        if(arr[i] >= arr[index])
-            index = i;
-    return index;
+bool strictlyEqual(const int list1[], const int list2[], int size) {
+    for(int i = 0; i < size; i++)
+        if(list1[i] != list2[i])
+            return false;
+    return true;
 }
 
 int main() {
-    double arr[15];
-    for(int i = 0; i < 15; i++)
-        cin >> arr[i];
-    cout << "Index of largest = " << indexOfLargestElement(arr, 15) << endl;
+    int n;
+    cin >> n;
+    int list1[n], list2[n];
+    for(int i = 0; i < n; i++)
+        cin >> list1[i];
+    for(int i = 0; i < n; i++)
+        cin >> list2[i];
+    if(strictlyEqual(list1, list2, n))
+        cout << "Two lists are strictly identical" << endl;
+    else
+        cout << "Two lists are not strictly identical" << endl;
     return 0;
 }
